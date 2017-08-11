293280c9638cbd7422101314299d0a5aca015f9d
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-08-12 00:51:55 +0200

    added dictionary

---
+++ common/dictionary.js
@@ -0,0 +1,14 @@
+const defaultFiatCurrency = 'PLN';
+
+const cryptoCurrencies = {
+    BTC: 'Bitcoin',
+    LTC: 'Litecoin',
+    ETH: 'Ethereum',
+    LSK: 'Lisk',
+    BCC: 'Bitcoin Cash',
+};
+
+module.exports = {
+    defaultFiatCurrency,
+    cryptoCurrencies,
+};
---
+++ common/index.js
@@ -0,0 +1,3 @@
+const dictionary = require('./dictionary');
+
+module.exports = Object.assign({}, dictionary);
