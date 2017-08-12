222d0a99203091b1a86a86bba292e060ab51368a
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-08-12 15:23:32 +0200

    moved routing to bitbay module

--- server/src/bitbay/index.js
+++ server/src/bitbay/index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+
 const public = require('./public');
 const private = require('./private');
 
-module.exports = Object.assign({}, public, private);
+const router = express.Router();
+
+router.get('/ticker', public.ticker);
+router.get('/ticker/refresh', public.tickerRefresh);
+router.get('/status', private.status);
+router.get('/status/refresh', private.statusRefresh);
+
+module.exports = {
+    router,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
-const bitbay = require('./bitbay');
+const { router: bitbayRouter } = require('./bitbay');
 
 const server = express();
-const router = express.Router();
-
-router.get('/ticker', bitbay.ticker);
-router.get('/ticker/refresh', bitbay.tickerRefresh);
-router.get('/status', bitbay.status);
-router.get('/status/refresh', bitbay.statusRefresh);
 
 server.use(cors());
 
-server.use('/api/v1/', router);
+server.use('/api/v1/', bitbayRouter);
 
 module.exports = {
     server,
