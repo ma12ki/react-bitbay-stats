b4de484444443bc8578f988698075b8307ed6dbd
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-08-15 12:19:48 +0200

    color-coded values

--- client/src/stats/StatsTable/cells/NumberCell/NumberCell.js
+++ client/src/stats/StatsTable/cells/NumberCell/NumberCell.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import './NumberCell.css';
+
+const getValueClassName = (value) => {
+    if (value > 0) {
+        return 'positive';
+    }
+    if (value < 0) {
+        return 'negative';
+    }
+    return 'neutral';
+};
+
 export default ({ fractionDigits = 2, children, ...rest }) => {
     const value = children !== null ? children.toLocaleString(undefined, {
         minimumFractionDigits: fractionDigits,
         maximumFractionDigits: fractionDigits,
     }) : 'N/A';
 
-    return (<td {...rest}>{value}</td>);
+    const valueClassName = getValueClassName(children);
+
+    return (<td {...rest}><span className={'NumberCell-' + valueClassName}>{value}</span></td>);
 };
